c1f179bb52cb9fca873b448f6934ee8fc45cb417
Artuto <artutogamer@gmail.com>
2017-08-07 03:02:59 -0500

    Update Kick.java

--- src/main/java/me/artuto/endless/commands/moderation/Kick.java
+++ src/main/java/me/artuto/endless/commands/moderation/Kick.java
@@ -60,7 +60,7 @@ public class Kick extends Command
         User author;
         author = event.getAuthor();
         
-        if(event.getArgs().isEmpty()
+        if(event.getArgs().isEmpty())
         {
             event.replyWarning("Invalid Syntax: "+event.getClient().getPrefix()+"kick @user | ID | nickname | username for *reason*");
             return;
