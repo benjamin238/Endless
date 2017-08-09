c5418822de0493a89bcb53d6689de916a89ac1a1
Artuto <artutogamer@gmail.com>
2017-08-09 14:55:47 -0500

    update version

--- src/main/java/me/artuto/endless/commands/moderation/Ban.java
+++ src/main/java/me/artuto/endless/commands/moderation/Ban.java
@@ -30,7 +30,6 @@ import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.User;
-import net.dv8tion.jda.core.exceptions.ErrorResponseException;
 
 /**
  *
