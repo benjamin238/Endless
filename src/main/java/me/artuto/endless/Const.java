3cf1d90fe0e7be85aa1b40b343c29839d6720b21
Artuto <artutogamer@gmail.com>
2017-08-18 14:17:13 -0500

    version bump

--- src/main/java/me/artuto/endless/Const.java
+++ src/main/java/me/artuto/endless/Const.java
@@ -24,7 +24,7 @@ package me.artuto.endless;
 
 public class Const
 {
-    public static String VERSION = ("1.4.1");
+    public static String VERSION = ("1.4.2");
     public static String DONE_E = ("✅");
     public static String WARN_E = ("⚠");
     public static String FAIL_E = ("❌");
