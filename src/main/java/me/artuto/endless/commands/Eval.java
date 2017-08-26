493808178dba32c8a43f89f40850a0764b0a3bc0
Artuto <artutogamer@gmail.com>
2017-08-26 18:19:27 -0500

    Update Eval.java

--- src/main/java/me/artuto/endless/commands/Eval.java
+++ src/main/java/me/artuto/endless/commands/Eval.java
@@ -100,7 +100,7 @@ public class Eval extends Command
             }
             
             Object out = engine.eval(
-                    "(function() {"
+                    "(function() { return "
                         + "with (imports) {"
                             + event.getArgs()
                         + "}"
