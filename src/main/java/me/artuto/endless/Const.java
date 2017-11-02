a8d4a806b803f69a6c70d77bdef05992f9dc3123
Artu <artutogamer@gmail.com>
2017-11-01 20:14:18 -0600

    version dump and update deps

--- src/main/java/me/artuto/endless/Const.java
+++ src/main/java/me/artuto/endless/Const.java
@@ -24,7 +24,7 @@ package me.artuto.endless;
 
 public class Const
 {
-    public static String VERSION = "2.0.6";
+    public static String VERSION = "2.1.0";
     public static String INVITE = "https://discord.gg/CXKfYW3";
     public static String GAME_0 = "[ENDLESS] loading...";
     public static String DEV = "`@Artuto#0424 | 264499432538505217`";
