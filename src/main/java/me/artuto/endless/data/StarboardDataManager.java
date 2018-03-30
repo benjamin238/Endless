01ae9410b18fb9fc9cbb9f0e6e733da6621f244c
Artuto <artutogamer@gmail.com>
2018-03-29 22:16:07 -0600

    fixed this too

--- src/main/java/me/artuto/endless/data/StarboardDataManager.java
+++ src/main/java/me/artuto/endless/data/StarboardDataManager.java
@@ -48,7 +48,7 @@ public class StarboardDataManager
         }
         catch(SQLException e)
         {
-            e.toString();
+            LOG.warn(e.toString());
             return false;
         }
     }
@@ -87,7 +87,6 @@ public class StarboardDataManager
 
             try(ResultSet results = statement.executeQuery(String.format("SELECT * FROM STARBOARD WHERE msg_id = %s", msg)))
             {
-<<<<<<< HEAD
                 if(results.next())
                 {
                     results.updateInt("star_amount", amount);
@@ -95,17 +94,11 @@ public class StarboardDataManager
                     return true;
                 }
                 else return false;
-=======
-                results.moveToInsertRow();
-                results.updateInt("star_amount", amount);
-                results.insertRow();
-                return true;
->>>>>>> a199b017a9eba77977231052365c89091864b3ab
             }
         }
         catch(SQLException e)
         {
-            e.toString();
+            LOG.warn(e.toString());
             return false;
         }
     }
