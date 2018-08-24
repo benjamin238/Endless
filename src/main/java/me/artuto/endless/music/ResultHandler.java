7a1dc73bf846b0e7442a57cc569f6ba41b6170fc
Artuto <artutogamer@gmail.com>
2018-08-24 13:43:46 -0500

    fixed queue position number

--- src/main/java/me/artuto/endless/music/ResultHandler.java
+++ src/main/java/me/artuto/endless/music/ResultHandler.java
@@ -97,9 +97,9 @@ public class ResultHandler implements AudioLoadResultHandler
 
     private void loadSingle(AudioTrack track, AudioPlaylist playlist)
     {
-        int pos = bot.musicTasks.putInQueue(track, event)+1;
+        int pos = bot.musicTasks.putInQueue(track, event)+2;
         String addMsg = FormatUtil.sanitize(event.getClient().getSuccess()+" Added the song **"+track.getInfo().title+"** (`"+
-                FormatUtil.formatTime(track.getDuration())+"`) "+(pos==-1?"to being playing!":"to the queue at position "+pos));
+                FormatUtil.formatTime(track.getDuration())+"`) "+(pos==1?"to begin playing!":"to the queue at position "+pos));
         if(playlist==null || !(ChecksUtil.hasPermission(event.getSelfMember(), event.getTextChannel(), Permission.MESSAGE_ADD_REACTION)))
             msg.editMessage(addMsg).queue();
         else
